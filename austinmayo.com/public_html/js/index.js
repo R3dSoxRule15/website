018ef2603edb315856e99c3a9069374c3daf31bf
Austin Mayo <austinmayo15@home.austinmayo.com>
2016-04-01 03:34:35 -0400

    Added two subdomains

--- austinmayo.com/public_html/js/index.js
+++ austinmayo.com/public_html/js/index.js
@@ -1,28 +1,71 @@
-$.fn.typewriter = function() {
-  this.each(function() {
-    var c = $(this),
-      b = c.html(),
-      a = 0,
-      d = 0;
-    c.html("");
-    var e = function() {
-      if ("<" == b.substring(a, a + 1)) {
-        var f = new RegExp(/<span class="instant"/),
-          g = new RegExp(/<span class="clear"/);
-        if (b.substring(a, b.length).match(f)) a += b.substring(a, 
-b.length).indexOf("</span>") + 7;
-        else if (b.substring(a, b.length).match(g)) d = a, a += 
-b.substring(a, b.length).indexOf("</span>") + 7;
-        else
-          for (;
-            ">" != b.substring(a, a + 1);) a++
+// ANIMATE AND POSITION PANELS
+
+function isInside(node, target) {
+  for (; node != null; node = node.parentNode) {
+    if (node == target) {
+      return true;
+    }
+  }
+}
+
+function setUpPanels() {
+  var pAll = document.querySelectorAll(".panel");
+  var pAbbr = [];
+  for (var f = 0; f < pAll.length; f++) {
+    if (pAll[f].nodeType == 1) {
+      pAbbr.push(pAll[f]);
+    }
+  }
+  for (var g = 0; g < pAbbr.length; g++) {
+    (function(h) {
+      pAbbr[h].addEventListener("mouseover", function(event) {
+        if (!isInside(event.relatedTarget, pAbbr[h])) {
+          var target = event.target;
+          while (target.className != "panel" && target.className != "panel inactive") {
+            target = target.parentNode;
+          }
+          makeActive(target);
+        }
+      });
+    })(g);
+  }
+  return pAbbr;
+}
+var panels = setUpPanels();
+
+function makeActive(target) {
+  for (var i = 0; i < panels.length; i++) {
+    panels[i].className = "panel";
+  }
+  target.className = "panel active";
+  for (var j = 0; j < panels.length; j++) {
+    if (panels[j].className == "panel active") {
+      var an = j;
+      var al = j * (70/9); // 70/9 = 7.77778
+      panels[j].style.left = al + "%";
+    }
+  }
+  for (var k = 0; k < panels.length; k++) {
+    if (panels[k].className != "panel active") {
+      panels[k].className = "panel inactive";
+      var il;
+      if (k < an) {
+        il = al - (an - k) * (70/9); // 70/9 = 7.77778
+        panels[k].style.left = il + "%";
+      } else if (k > an) {
+        il = al + 40 / 2 + (k - an) * (70/9); // 70/9 = 7.77778
+        panels[k].style.left = il + "%";
       }
-      c.html(b.substring(d, a++) + (a & 1 ? "_" : ""));
-      a >= b.length || setTimeout(e, 70 + 100 *
-        Math.random())
-    };
-    e()
-  });
-  return this
-};
-$(".terminal").typewriter();
+    }
+  }
+}
+
+var container = document.getElementById("container");
+container.addEventListener("mouseout", function(event) {
+  if (!isInside(event.relatedTarget, container)) {
+    for (var l = 0; l < panels.length; l++) {
+      panels[l].className = "panel";
+      panels[l].style.left = "";
+    }
+  }
+});
